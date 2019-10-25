01754c11705f1f0b7ecf023a305c7e6f65584e5a
Chauhan, Gaurav (CORP) <Gaurav.Chauhan@adp.com>
2019-10-25 13:25:09 -0400

    initial commit

---
+++ src/main/java/com/adp/translation/JerseyConfig.java
@@ -0,0 +1,13 @@
+package com.adp.translation;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+
+        register(ExampleResource.class);
+    }
+}
